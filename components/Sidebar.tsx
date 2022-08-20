9dfb47f66bec8884bb02b2014cff71f1dae62737
Toby Chambers <74092151+Toby-Chambers@users.noreply.github.com>
2022-08-20 17:25:53 +0100

    Commit

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -14,6 +14,7 @@ import {
   faBars,
 } from "@fortawesome/free-solid-svg-icons";
 import SidebarSection from "./SidebarSection";
+import Link from "next/link";
 
 const renderActionsSection = (user: any) => {
   if (user.permissionLevel > 1)
@@ -21,10 +22,10 @@ const renderActionsSection = (user: any) => {
       <SidebarSection
         header="Actions"
         items={[
-          { name: "Manage Events", icon: faCalendar },
-          { name: "Manage Personnel", icon: faUsers },
-          { name: "Records", icon: faTable },
-          { name: "Data Pull", icon: faFileCsv },
+          { name: "Manage Events", icon: faCalendar, page: "manageevents" },
+          { name: "Manage Personnel", icon: faUser, page: "managepersonnel" },
+          { name: "Records", icon: faTable, page: "records" },
+          { name: "Data Pull", icon: faFileCsv, page: "datapull" },
         ]}
       />
     );
@@ -35,10 +36,10 @@ const Sidebar = ({ user }: any) => {
       <SidebarSection
         header="Main"
         items={[
-          { name: "Home", icon: faHouse },
-          { name: "Profile", icon: faUser },
-          { name: "Attendance", icon: faListOl },
-          { name: "Events", icon: faCalendar },
+          { name: "Home", icon: faHouse, page: "/" },
+          { name: "Profile", icon: faUser, page: "profile" },
+          { name: "Attendance", icon: faListOl, page: "attendance" },
+          { name: "Events", icon: faCalendar, page: "events" },
         ]}
       />
       <div className={styles.SidebarDivider}></div>
@@ -48,20 +49,24 @@ const Sidebar = ({ user }: any) => {
       <SidebarSection
         header="Legal"
         items={[
-          { name: "Terms of Service", icon: faAnchor },
-          { name: "Privacy Policy", icon: faAnchor },
+          { name: "Terms of Service", icon: faAnchor, page: "termsofservice" },
+          { name: "Privacy Policy", icon: faAnchor, page: "privacypolicy" },
         ]}
       />
 
       <div className={styles.SidebarFooter}>
-        <div className={styles.SidebarItem}>
-          <FontAwesomeIcon icon={faCircleInfo} className={styles.SidebarItemIcon} />
-          <h3 className={styles.SidebarItemText}>Support</h3>
-        </div>
-        <div className={styles.SidebarItem}>
-          <FontAwesomeIcon icon={faLock} className={styles.SidebarItemIcon} />
-          <h3 className={styles.SidebarItemText}>Logout</h3>
-        </div>
+        <Link href="/">
+          <div className={styles.SidebarItem}>
+            <FontAwesomeIcon icon={faCircleInfo} className={styles.SidebarItemIcon} />
+            <h3 className={styles.SidebarItemText}>Support</h3>
+          </div>
+        </Link>
+        <Link href="/">
+          <div className={styles.SidebarItem}>
+            <FontAwesomeIcon icon={faLock} className={styles.SidebarItemIcon} />
+            <h3 className={styles.SidebarItemText}>Logout</h3>
+          </div>
+        </Link>
       </div>
     </div>
   );
--- components/SidebarSection.tsx
+++ components/SidebarSection.tsx
@@ -1,4 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 import styles from "../styles/Sidebar.module.css";
 const SidebarSection = ({ header, items }: any) => {
   return (
@@ -6,10 +7,12 @@ const SidebarSection = ({ header, items }: any) => {
       <h3 className={styles.SidebarSectionHeader}>{header}</h3>
       {items.map((item: any, index: number) => {
         return (
-          <div className={styles.SidebarItem} key={`sidebar-${item.name}`}>
-            <FontAwesomeIcon icon={item.icon} className={styles.SidebarItemIcon} />
-            <h3 className={styles.SidebarItemText}>{item.name}</h3>
-          </div>
+          <Link href={`/dashboard/${item.page}`}>
+            <div className={styles.SidebarItem} key={`sidebar-${item.name}`}>
+              <FontAwesomeIcon icon={item.icon} className={styles.SidebarItemIcon} />
+              <h3 className={styles.SidebarItemText}>{item.name}</h3>
+            </div>
+          </Link>
         );
       })}
     </div>
